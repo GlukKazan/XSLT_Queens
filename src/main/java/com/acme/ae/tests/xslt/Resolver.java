4e99f424942683a551b28e88c24ed7430c7d4de3
GlukKazan <glukkazan@gmail.com>
2013-11-21 20:21:45 +0400

    sources

---
+++ src/main/java/com/acme/ae/tests/xslt/Resolver.java
@@ -0,0 +1,15 @@
+package com.acme.ae.tests.xslt;
+
+import java.io.File;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.stream.StreamSource;
+
+public class Resolver implements URIResolver {
+
+	public Source resolve(String href, String base) throws TransformerException {
+		return new StreamSource(new File("xsl/" + href));
+	}
+}
